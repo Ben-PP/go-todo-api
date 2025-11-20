e359164a8258adaef019ead939fe50c31402667a
Karel Parkkola <github.snout266@passmail.net>
2025-11-20 10:31:53 +0200

    feat: Read list shares

---
+++ features/todo/read_shares.go
@@ -0,0 +1,73 @@
+package todo
+
+import (
+	"runtime"
+
+	"go-todo/gterrors"
+	"go-todo/logging"
+	"go-todo/schemas"
+	"go-todo/util/database"
+	"go-todo/util/mycontext"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (controller *TodoController) ReadShares(ctx *gin.Context) {
+	requesterId, requesterUsername, _, err := mycontext.GetTokenVariables(ctx)
+	if err != nil {
+		_, file, line, _ := runtime.Caller(0)
+		mycontext.CtxAddGtInternalError("failed to get claims from jwt", file, line, err, ctx)
+		return
+	}
+
+	listID := ctx.Param("listID")
+	reqUser, err := database.GetUserById(controller.db, requesterId, ctx)
+	if err != nil {
+		logging.LogSecurityEvent(
+			logging.SecurityScoreLow,
+			logging.SecurityEventJwtUserUnknown,
+			ctx.FullPath(),
+			requesterUsername,
+			ctx.ClientIP(),
+		)
+		return
+	}
+
+	list, err := controller.db.GetList(ctx, listID)
+	if err != nil {
+		_, file, line, _ := runtime.Caller(0)
+		mycontext.CtxAddGtInternalError("failed to get list", file, line, err, ctx)
+		return
+	}
+
+	if list.UserID != reqUser.ID && !reqUser.IsAdmin {
+		logging.LogSecurityEvent(
+			logging.SecurityScoreLow,
+			logging.SecurityEventForbiddenAction,
+			ctx.FullPath(),
+			listID,
+			reqUser.ID,
+		)
+		ctx.Error(gterrors.ErrForbidden).SetType(gin.ErrorTypePublic)
+		return
+	}
+
+	shares, err := controller.db.ReadShares(ctx, listID)
+	if err != nil {
+		_, file, line, _ := runtime.Caller(0)
+		mycontext.CtxAddGtInternalError("failed to read shares", file, line, err, ctx)
+		return
+	}
+
+	responseShares := make([]schemas.ResponseShare, 0, len(shares))
+	for _, share := range shares {
+		responseShares = append(responseShares, schemas.ResponseShare{
+			ListID:   share.ListID,
+			UserID:   share.UserID,
+			Username: share.Username,
+		})
+	}
+
+	// TODO Add logging. Add after object event logging has been reworked.
+	ctx.JSON(200, responseShares)
+}
--- schemas/share.schema.go
+++ schemas/share.schema.go
@@ -3,3 +3,9 @@ package schemas
 type CreateShare struct {
 	UserName string `json:"username" binding:"required"`
 }
+
+type ResponseShare struct {
+	ListID   string `json:"list_id"`
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+}
