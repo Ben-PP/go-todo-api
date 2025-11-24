9719605200c3b08578dbb599f1bba59bcdf3a29c
Karel Parkkola <github.snout266@passmail.net>
2025-11-24 12:18:32 +0200

    perf: Use single update statement

--- db/migrations/000002_add_share_id.up.sql
+++ db/migrations/000002_add_share_id.up.sql
@@ -2,14 +2,7 @@ BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 ALTER TABLE list_shares ADD COLUMN id TEXT;
 
-DO $$
-DECLARE
-    share RECORD;
-BEGIN
-    FOR share IN SELECT * FROM list_shares LOOP
-        UPDATE list_shares SET id = uuid_generate_v4() WHERE list_id = share.list_id AND user_id = share.user_id;
-    END LOOP;
-END $$;
+UPDATE list_shares SET id = uuid_generate_v4()::TEXT;
 
 ALTER TABLE list_shares DROP CONSTRAINT list_shares_pkey;
 ALTER TABLE list_shares ADD PRIMARY KEY (id);
