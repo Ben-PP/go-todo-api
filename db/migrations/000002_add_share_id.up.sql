686fa8ab60c5e2647afc3935e463b511576049b5
Karel Parkkola <github.snout266@passmail.net>
2025-11-24 12:25:25 +0200

    perf: Optimized add_share_id migration

--- db/migrations/000002_add_share_id.up.sql
+++ db/migrations/000002_add_share_id.up.sql
@@ -1,9 +1,9 @@
-BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 ALTER TABLE list_shares ADD COLUMN id TEXT;
 
 UPDATE list_shares SET id = uuid_generate_v4()::TEXT;
 
+ALTER TABLE list_shares ALTER COLUMN id SET NOT NULL;
+ALTER TABLE list_shares ALTER COLUMN id SET UNIQUE;
 ALTER TABLE list_shares DROP CONSTRAINT list_shares_pkey;
 ALTER TABLE list_shares ADD PRIMARY KEY (id);
-COMMIT;
