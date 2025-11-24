1db046da8431d6b384dd81291763c44b503fe398
Karel Parkkola <github.snout266@passmail.net>
2025-11-24 12:33:01 +0200

    fix: remove unique constraint

--- db/migrations/000002_add_share_id.up.sql
+++ db/migrations/000002_add_share_id.up.sql
@@ -4,6 +4,5 @@ ALTER TABLE list_shares ADD COLUMN id TEXT;
 UPDATE list_shares SET id = uuid_generate_v4()::TEXT;
 
 ALTER TABLE list_shares ALTER COLUMN id SET NOT NULL;
-ALTER TABLE list_shares ALTER COLUMN id SET UNIQUE;
 ALTER TABLE list_shares DROP CONSTRAINT list_shares_pkey;
 ALTER TABLE list_shares ADD PRIMARY KEY (id);
